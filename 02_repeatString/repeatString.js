d4ea4906302743aa2ac9080124a7e31cf9d8b46f
DracoPi <dracopiart@gmail.com>
2023-05-18 14:17:02 -0400

    Exercise 2 Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let stringC = '';
+    let i = 0; 
+    if (num < 0){
+        stringC += "ERROR";
+    }
 
+    for (i; i < num; i++){
+        stringC += word;
+       
+    } 
+    return stringC;
 };
 
 // Do not edit below this line
