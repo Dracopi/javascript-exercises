559fcc1f72f4ff1500f3e68f125c36a98854e66f
DracoPi <dracopiart@gmail.com>
2023-05-21 16:54:48 -0400

    First part exercise 5 completed, must complete the "ERROR" part.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(...arg) {
+// Crear un loop que añada un número más cada vez
+// hasta llegar a 4, pero que a medida que añada, también los sume.
+// y ese es el resultado que necesitamos
+let maxArg = Math.max(...arg);
+let minArg = Math.min(...arg);
+let sum = 1;
+// if (...arg === Number){
+// console.log("ERROR")
+// }
+// else {
+    do {
+        minArg++;
+        sum += minArg;
+        
+        console.log(sum);
+    }    while (minArg < maxArg)
+    
+    return sum;
 
 };
 
