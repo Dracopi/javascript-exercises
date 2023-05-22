08af04c4b1809a02642c07a9ae214d985933bd9e
DracoPi <dracopiart@gmail.com>
2023-05-22 09:58:38 -0400

    My version of the exercise, could'nt get error on non number to work
    turns out that i needed to do .isInteger, and i could just use min, max as variable
    I still prefer my version of that, but will try the other one

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,22 +1,33 @@
-const sumAll = function(...arg) {
+const sumAll = function(...args) {
 // Crear un loop que añada un número más cada vez
 // hasta llegar a 4, pero que a medida que añada, también los sume.
 // y ese es el resultado que necesitamos
-let maxArg = Math.max(...arg);
-let minArg = Math.min(...arg);
+let maxArg = Math.max(...args);
+let minArg = Math.min(...args);
 let sum = 1;
+let er = 'ERROR';
+// console.log(maxArg)
+// console.log(minArg)
 // if (...arg === Number){
 // console.log("ERROR")
 // }
-// else {
-    do {
-        minArg++;
-        sum += minArg;
-        
-        console.log(sum);
-    }    while (minArg < maxArg)
+for (const arg of args) {
+    if (typeof arg !== "number" || arg < 0) { 
+        return er;
+   
+}
+
+else { do {
+    minArg++;
+    sum += minArg;
     
-    return sum;
+  //   console.log(sum);
+}    while (minArg < maxArg )
+
+return sum;
+    
+    
+}}
 
 };
 
