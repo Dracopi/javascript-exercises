2208328822ad612ee489202558458ae66bf7c06c
DracoPi <dracopiart@gmail.com>
2023-05-23 09:38:17 -0400

    I could'nt work it out in my way, so i accept the loss.
    The exercise is completed and i (believe) understand how it work

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,35 +1,38 @@
-const sumAll = function(...args) {
-// Crear un loop que añada un número más cada vez
-// hasta llegar a 4, pero que a medida que añada, también los sume.
-// y ese es el resultado que necesitamos
-let maxArg = Math.max(...args);
-let minArg = Math.min(...args);
-let sum = 1;
-let er = 'ERROR';
-// console.log(maxArg)
-// console.log(minArg)
-// if (...arg === Number){
-// console.log("ERROR")
-// }
-for (const arg of args) {
-    if (typeof arg !== "number" || arg < 0) { 
-        return er;
-   
-}
-
-else { do {
+const sumAll = function(min, max) {
+let minArg = min;
+let sum = 0;
+if (!Number.isInteger(min) || !Number.isInteger(max))  return "ERROR";
+if (min < 0 || max < 0) return "ERROR";
+for (sum; min < max +1; ) {
     minArg++;
     sum += minArg;
-    
-  //   console.log(sum);
-}    while (minArg < maxArg )
+return sum; 
+}};
 
-return sum;
+    // Crear un loop que añada un número más cada vez
+    // hasta llegar a 4, pero que a medida que añada, también los sume.
+    // y ese es el resultado que necesitamos
+    // let maxArg = Math.max(...args);
+    // let minArg = Math.min(...args);
+    // let sum = 1;
+    // let er = 'ERROR';
+    // for (const arg of args) {
+    //     if (typeof arg !== "number" || arg < 0) { 
+    //         return er;
+       
+    // }
     
+    // else { do {
+    //     minArg++;
+    //     sum += minArg;
+        
+    //   //   console.log(sum);
+    // }    while (minArg < maxArg )
     
-}}
-
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+    // return sum;
+        
+        
+    // }}
+    
+    // };
+    
